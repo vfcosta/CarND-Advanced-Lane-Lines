5e52712629031d7a14cb81f91f8ad025791a45f6
Victor Costa <vfcosta@gmail.com>
2017-01-31 19:43:28 -0300

    Apply perspective transformation

--- lanelines/camera.py
+++ lanelines/camera.py
@@ -13,6 +13,8 @@ class Camera:
         self.dist_coeffs = None
         self.rvecs = None
         self.tvecs = None
+        self.perspective_matrix = None
+        self.inverse_perspective_matrix = None
 
     def calibrate(self, path="../camera_cal/*.jpg", corners_x=9, corners_y=6, ignore_missing=False):
         objpoints, imgpoints, self.shape = self.detect_image_points(path, corners_x=corners_x, corners_y=corners_y, ignore_missing=ignore_missing)
@@ -23,12 +25,28 @@ class Camera:
     def undistort(self, image):
         return cv2.undistort(image, self.camera_matrix, self.dist_coeffs, None, self.camera_matrix)
 
+    def detect_perspective(self, image, source_points, dest_points=None, offset_x=400, offset_y=100):
+        if dest_points is None:
+            dest_points = np.float32([(offset_x, offset_y), (image.shape[1] - offset_x, offset_y),
+                                      (image.shape[1] - offset_x, image.shape[0]), (offset_x, image.shape[0])])
+
+        self.perspective_matrix = cv2.getPerspectiveTransform(source_points, dest_points)
+        self.inverse_perspective_matrix = cv2.getPerspectiveTransform(dest_points, source_points)
+
+    def unwarp(self, image):
+        return cv2.warpPerspective(image, self.perspective_matrix,
+                                   (image.shape[1], image.shape[0]), flags=cv2.INTER_LINEAR)
+
     def save(self, filename="camera.p"):
-        pickle.dump(self, open(filename, "wb"))
+        pickle.dump(vars(self), open(filename, "wb"))
 
     @classmethod
     def load(cls, filename="camera.p"):
-        return pickle.load(open(filename, "rb"))
+        camera = Camera()
+        attributes = pickle.load(open(filename, "rb"))
+        for k, v in attributes.items():
+            setattr(camera, k, v)
+        return camera
 
     @staticmethod
     def draw_chessboard(image, corners_x, corners_y):
@@ -92,6 +110,7 @@ if __name__ == '__main__':
         camera = Camera()
         camera.calibrate()
         camera.save()
+
     camera = Camera.load()
     for image_name, corners in [("calibration1", (9, 5)), ("calibration2", (8, 6)), ("calibration4", (6, 5))]:
         print("undistort", image_name)
@@ -100,3 +119,16 @@ if __name__ == '__main__':
         undistorted = camera.undistort(image)
         cv2.imwrite("../output_images/"+image_name+"_chessboard.jpg", chess)
         cv2.imwrite("../output_images/"+image_name+"_undistorted.jpg", undistorted)
+
+    if camera.perspective_matrix is None:
+        print("detect perspective")
+        image = cv2.imread("../output_images/straight_lines1_perspective.jpg")
+        source_points = [(581, 461), (703, 461), (1064, 692), (245, 692)]
+        camera.detect_perspective(image, source_points=np.float32(source_points))
+        camera.save()
+
+    for filename in ["../output_images/straight_lines1_perspective.jpg", "../test_images/test2.jpg"]:
+        print("unwarp", filename)
+        output_filename = filename.replace("test_images", "output_images").replace(".jpg", "_unwarped.jpg")
+        image = cv2.imread(filename)
+        cv2.imwrite(output_filename, camera.unwarp(camera.undistort(image)))
