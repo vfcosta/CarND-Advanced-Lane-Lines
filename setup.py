99264e7f127ededf57f730244523db2fd38a077f
Victor Costa <vfcosta@gmail.com>
2017-02-06 22:09:00 -0300

    Add project configuration

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='lanelines',
+   version='1.0',
+   description='Advanced lane lines',
+   author='Victor Costa',
+   author_email='vfcosta@gmail.com',
+   packages=['lanelines'],
+   install_requires=[]
+)
